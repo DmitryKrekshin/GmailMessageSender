c8e487b6f14ed28c33d62295abae6328a88535cc
DmitryKrekshin <dima.krejshin@gmail.com>
2023-10-14 17:07:59 +0300

    add project files

---
+++ Main.py
@@ -0,0 +1,23 @@
+import configparser
+import smtplib
+from email.mime.text import MIMEText
+
+config = configparser.ConfigParser()
+config.read("Config.ini")
+
+subject = config["message"]["subject"]
+email_text = config["message"]["text"]
+
+GMAIL_USERNAME = config["email_sender"]["userName"]
+GMAIL_APP_PASSWORD = config["email_sender"]["password"]
+
+recipients = config["email_receiver"]["email"].split(";")
+msg = MIMEText(email_text)
+msg["Subject"] = subject
+msg["To"] = ", ".join(recipients)
+msg["From"] = f"{GMAIL_USERNAME}@gmail.com"
+
+smtp_server = smtplib.SMTP_SSL("smtp.gmail.com", 465)
+smtp_server.login(GMAIL_USERNAME, GMAIL_APP_PASSWORD)
+smtp_server.sendmail(msg["From"], recipients, msg.as_string())
+smtp_server.quit()
---
+++ README.md
@@ -0,0 +1,8 @@
+# GmailMessageSender
+
+Hey there!
+
+I created this program to send messages from my gmail account to anothers accouts.
+Only you need to fill Config.ini file with your sender email, receiver emails and message data (subject and text).
+
+Also you must sure your sender email has application password, detailes here: https://support.google.com/mail/answer/185833?hl=en
