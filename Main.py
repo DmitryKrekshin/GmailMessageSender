804c9876167e2ade42895a798315c4261c208c74
DmitryKrekshin <dima.krejshin@gmail.com>
2023-11-19 11:56:51 +0300

    add the ability to send message text via the command line

--- Main.py
+++ Main.py
@@ -1,12 +1,16 @@
 import configparser
 import smtplib
+import sys
 from email.mime.text import MIMEText
 
 config = configparser.ConfigParser()
 config.read("Config.ini")
 
 subject = config["message"]["subject"]
-email_text = config["message"]["text"]
+if len(sys.argv) > 1:
+    email_text = sys.argv[1]
+else:
+    email_text = config["message"]["text"]
 
 GMAIL_USERNAME = config["email_sender"]["userName"]
 GMAIL_APP_PASSWORD = config["email_sender"]["password"]
